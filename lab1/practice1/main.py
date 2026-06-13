7570a764ad8d5afc8ea14c61863d670ffd920141
zarina-0-0 <b.zarina_2601@mail.ru>
2026-06-13 23:15:06 +0300

    practice1-аннотация

--- lab1/practice1/main.py
+++ lab1/practice1/main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI
-from models import temp_bd
+from models import temp_bd, Workout
+from typing_extensions import TypedDict
+
 
 app = FastAPI()
 
 
 @app.get("/workouts")
-def workouts_list():
+def workouts_list() -> list[Workout]:
     return temp_bd
 
 
 @app.get("/workout/{workout_id}")
-def workout_list(workout_id: int):
+def workout_list(workout_id: int) -> list[Workout]:
     return [workout for workout in temp_bd if workout.get("id") == workout_id]
 
 
 @app.post("/workout")
-def workout_add(workout: dict):
+def workout_add(workout: Workout) -> TypedDict('Response', {"status": int, "data": Workout}):
     temp_bd.append(workout)
     return {"status": 200, "data": workout}
 
@@ -30,7 +32,7 @@ def workout_delete(workout_id: int):
 
 
 @app.put("/workout{workout_id}")
-def warrior_update(workout_id: int, workout: dict):
+def warrior_update(workout_id: int, workout: Workout) -> list[Workout]:
     for i, work in enumerate(temp_bd):
         if work.get("id") == workout_id:
             temp_bd[i] = workout
--- lab1/practice1/models.py
+++ lab1/practice1/models.py
@@ -4,16 +4,54 @@ from typing import Optional, List
 from pydantic import BaseModel
 
 
-temp_bd = [
-{
+class WorkoutCategory(Enum):
+    strength = "силовая"
+    cardio = "кардио"
+    stretching = "растяжка"
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+    weight: float
+    height: float
+    level: Optional[int] = None
+
+
+class Exercise(BaseModel):
+    id: int
+    name: str
+    sets: int
+    reps: Optional[int] = None
+    duration: Optional[int] = None
+    weight: Optional[float] = None
+    description: str
+
+
+class Workout(BaseModel):
+    id: int
+    category: WorkoutCategory
+    title: str
+    date: str
+    duration: int
+    user: User
+    notes: Optional[str] = None
+    exercises: List[Exercise]
+
+
+temp_bd = [{
     "id": 1,
     "category": "силовая",
-    "tittle": "пресс + руки",
+    "title": "пресс + руки",
     "date": "2026-04-22",
     "duration": 25,
     "user": {
         "id": 1,
-        "name": "Зарина Биктагирова",
+        "name": "Аня Смирнова",
+        "age": 22,
+        "weight": 57.5,
+        "height": 158,
         "level": 3
     },
     "notes": "тренировка прошла легко но сильно вспотела)",
@@ -39,15 +77,18 @@ temp_bd = [
 
         }]
 },
-{
+    {
     "id": 2,
     "category": "силовая",
-    "tittle": "ноги",
+    "title": "ноги",
     "date": "2026-05-22",
     "duration": 25,
     "user": {
         "id": 1,
         "name": "Даша",
+        "age": 29,
+        "weight": 58.8,
+        "height": 162,
         "level": 2
     },
     "notes": "устала, но не сильно",
@@ -62,4 +103,5 @@ temp_bd = [
             "description": "спина прямая, угол в коленях 90 градусов"
 
         }]
-},]
+},
+]
